23c464b0caab5f2452dd062cff411c96c804dce4
Anuj Dhavalikar <chronal@chronal.space>
2025-11-12 02:16:20 +1100

    Fix ifdef for step.h

--- include/step.h
+++ include/step.h
@@ -1,5 +1,5 @@
-#ifndef GAME_H
-#define GAME_H
+#ifndef STEP_H
+#define STEP_H
 
 #include "state.h"
 
@@ -9,4 +9,4 @@ void paddle_step(paddle* p, Uint64 ticks);
 void ball_init(ball* b);
 void ball_step(ball* b, Uint64 ticks);
 
-#endif
+#endif /* STEP_H */
